fccdff9522349abb6b1bcb87c4b2a88b17d6f2c6
Your Name <you@example.com>
2026-02-10 16:44:41 +0100

    `Revert changes to crypto API endpoints and balance display in TODO.md, crypto.html, and app.js`

--- public/js/app.js
+++ public/js/app.js
@@ -97,7 +97,7 @@ const UserPayClient = (function () {
   /* ================= CRYPTO ================= */
 
   async function getCryptoBalance() {
-    const res = await client.get("/crypto/balance");
+    const res = await client.get("/api/crypto/balance");
     const data = res.data;
     if (data && data.cryptoBalances) {
       return data;
@@ -107,22 +107,22 @@ const UserPayClient = (function () {
   }
 
   async function cryptoTopup(currency, amount) {
-    const res = await client.post("/crypto/topup", { cryptoType: currency, amount });
+    const res = await client.post("/api/crypto/topup", { cryptoType: currency, amount });
     return res.data;
   }
 
   async function getCryptoTransactions() {
-    const res = await client.get("/crypto/transactions");
+    const res = await client.get("/api/crypto/transactions");
     return Array.isArray(res.data) ? res.data : [];
   }
 
   async function sendCrypto(toAddress, currency, amount, password, memo) {
-    const res = await client.post("/crypto/send", { toAddress, cryptoType: currency, amount, password, memo });
+    const res = await client.post("/api/crypto/send", { toAddress, cryptoType: currency, amount, password, memo });
     return res.data;
   }
 
   async function confirmCryptoSend(transactionId, otp) {
-    const res = await client.post("/crypto/send/confirm", { transactionId, otp });
+    const res = await client.post("/api/crypto/send/confirm", { transactionId, otp });
     return res.data;
   }
 
